f35a9b219f8c98a9b1b4afc5a788515013db994d
Aditya Purandare <aditya.p1993@hotmail.com>
2016-03-07 21:02:43 -0600

    Base file for model

---
+++ CPOutfitters/Artifact.swift
@@ -0,0 +1,14 @@
+//
+//  Artifact.swift
+//  CPOutfitters
+//
+//  Created by Aditya Purandare on 07/03/16.
+//  Copyright © 2016 SnazzyLLama. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class Artifact: PFObject {
+
+}
