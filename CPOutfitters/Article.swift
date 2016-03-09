0ea53bb25c29f683e5d7edf1a24344bc42058838
Aditya Purandare <aditya.p1993@hotmail.com>
2016-03-08 21:28:26 -0600

    Implement PFLogin view controller

--- CPOutfitters/Article.swift
+++ CPOutfitters/Article.swift
@@ -46,5 +46,46 @@ class Article: PFObject {
         }
         return articles
     }
+    
+    class func getPFFileFromImage(image: UIImage?) -> PFFile? {
+        if let image = image {
+            if let imageData = UIImagePNGRepresentation(image) {
+                return PFFile(name: "image.png", data: imageData)
+            }
+        }
+        return nil
+    }
+    class func fetchArticlesWithCompletion(completion completion:([PFObject]?, NSError?) -> ()) {
+        let query = PFQuery(className: "Article")
+        query.limit = 40
+        query.findObjectsInBackgroundWithBlock(completion)
+    }
+    class func loadProfileImageWithCompletion(params: NSDictionary, completion:([PFObject]?, NSError?) -> ()) {
+        let obj = params
+        var userName: String?
+        for (key, value) in obj {
+            userName = value as? String
+        }
+        print(userName!)
+        let query = PFQuery(className: "_User")
+        query.whereKey("username", equalTo: userName!)
+        query.limit = 20
+        query.findObjectsInBackgroundWithBlock(completion)
+    }
+    class func searchPostsWithCompletion(params: NSDictionary, completion:([PFObject]?, NSError?) -> ()) {
+        let obj = params
+        var searchKey: String?
+        var searchValue: String?
+        for (key, value) in obj {
+            searchKey = key as? String
+            searchValue = value as? String
+        }
+        print("Key: \(searchKey!), Value: \(searchValue!)")
+        let query = PFQuery(className: "Post")
+        query.whereKey(searchKey!, containsString: searchValue!)
+        query.orderByDescending("timestamp")
+        query.limit = 20
+        query.findObjectsInBackgroundWithBlock(completion)
+    }
 
 }
