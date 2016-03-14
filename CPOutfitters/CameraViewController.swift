c601383452d5601f65bf2243892f41313224b4e2
Cory Thompson <cory.thompson527@tamu.edu>
2016-03-14 16:44:05 -0500

    Began the process of setting up the camera.

--- CPOutfitters/CameraViewController.swift
+++ CPOutfitters/CameraViewController.swift
@@ -8,15 +8,63 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var tagsTextField: UITextField!
+    @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var averageColorImageView: UIImageView!
+    var libraryHasBeenViewed: Bool!
+    var vc: UIImagePickerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        libraryHasBeenViewed = false
+        vc = UIImagePickerController()
         
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        if let _ = posterImageView.image {
+            posterImageView.hidden = false
+            averageColorImageView.hidden = false
+            tagsTextField.hidden = false
+        } else {
+            averageColorImageView.hidden = true
+            tagsTextField.hidden = true
+            posterImageView.hidden = true
+        }
+        
+        
+    }
+    
+    func imagePickerController(picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            // Get the image captured by the UIImagePickerController
+            let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+            
+            // Do something with the images (based on your use case)
+            posterImageView.image = editedImage
+            // Dismiss UIImagePickerController to go back to your original view controller
+            dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    @IBAction func onCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+            vc.sourceType = .Camera
+            self.presentViewController(vc, animated: true, completion: nil)
+        } else {
+            print("camera not available")
+        }
+        
+    }
+    
+    @IBAction func onLibraryButton(sender: AnyObject) {
+        vc.sourceType = .PhotoLibrary
+        self.presentViewController(vc, animated: true, completion: nil)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
