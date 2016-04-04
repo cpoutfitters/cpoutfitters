75eaad7cfcbeaa9eb18e10d03919ddfab950fc8f
Aditya Purandare <aditya.p1993@hotmail.com>
2016-04-04 00:29:38 -0500

    Add base structure

---
+++ CPOutfitters/OutfitsCell.swift
@@ -0,0 +1,26 @@
+//
+//  OutfitsCell.swift
+//  CPOutfitters
+//
+//  Created by Aditya Purandare on 04/04/16.
+//  Copyright © 2016 SnazzyLLama. All rights reserved.
+//
+
+import UIKit
+
+class OutfitsCell: UITableViewCell {
+
+    @IBOutlet weak var categoryLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
