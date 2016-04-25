12a0e43a56ceedab410015ddcc3ede089258f074
Aditya Purandare <aditya.p1993@hotmail.com>
2016-04-25 09:54:20 -0500

    Cleanup UI & save profile picture

--- CPOutfitters/ProfileViewController.swift
+++ CPOutfitters/ProfileViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Parse
 
-class ProfileViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var userhandleLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
@@ -28,8 +28,6 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate, UIColle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
 
         ParseClient.sharedInstance.getUser(["owner": PFUser.currentUser()!]) { (user: PFUser?, error:NSError?) in
             //print(user)
@@ -43,52 +41,7 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate, UIColle
             print(countFabrics)
             self.articleCount.text = countFabrics
         }
-        ParseClient.sharedInstance.fetchArticles(["type":"top"]) { (articleObjects:[Article]?, error: NSError?) in
-            if let articleObjects = articleObjects {
-                    self.articles[0] = articleObjects
-            } else {
-                let errorString = error!.userInfo["error"] as? NSString
-                print("Error message: \(errorString)")
-            }
-        }
-        ParseClient.sharedInstance.fetchArticles(["type":"bottom"]) { (articleObjects:[Article]?, error: NSError?) in
-            if let articleObjects = articleObjects {
-                self.articles[1] = articleObjects
-            } else {
-                let errorString = error!.userInfo["error"] as? NSString
-                print("Error message: \(errorString)")
-            }
-        }
-        ParseClient.sharedInstance.fetchArticles(["type":"footwear"]) { (articleObjects:[Article]?, error: NSError?) in
-            if let articleObjects = articleObjects {
-                self.articles[2] = articleObjects
-            } else {
-                let errorString = error!.userInfo["error"] as? NSString
-                print("Error message: \(errorString)")
-            }
-        }
-//        print(self.articlesBottom)
-//        for article in articlesBottom! {
-//            combinedArticles?.append(article)
-//        }
     }
-    
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 3
-    }
-    
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return (articles[section].count > 0) ? articles[section].count : 0
-    }
-    
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("FabricViewCell", forIndexPath: indexPath) as! FabricViewCell
-        
-        cell.article = articles[indexPath.section][indexPath.row]
-        
-        return cell
-    }
-
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -124,15 +77,12 @@ class ProfileViewController: UIViewController, UICollectionViewDelegate, UIColle
             self.userProfileImageView.image = resizedImage
             self.userProfileImageView.layer.cornerRadius = self.userProfileImageView.frame.size.width / 2
             self.userProfileImageView.clipsToBounds = true
-     
 
-//            var currentUser = PFUser.currentUser()
-//            currentUser!.refreshInBackgroundWithBlock { (object, error) -> Void in
-//                currentUser!.fetchIfNeededInBackgroundWithBlock { (result, error) -> Void in
-//                    currentUser!.objectForKey("profileImage") =  Article.getPFFileFromImage(resizedImage!)                    
-//                }
-//            }
-        }
+
+            let user = PFUser.currentUser()
+            user!["profilePicture"] = Article.getPFFileFromImage(resizedImage)
+            user!.saveInBackground()
+         }
         
         // Dismiss UIImagePickerController to go back to your original view controller
         dismissViewControllerAnimated(true, completion: nil)
