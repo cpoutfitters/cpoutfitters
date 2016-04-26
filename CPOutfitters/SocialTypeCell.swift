bc382bfa3856c7c73e78ef977e8e02399d20ac81
Cory Thompson <cory.thompson527@tamu.edu>
2016-04-25 20:12:05 -0500

    Begin creating SocialViewController and needs to segue from OutfitViewController.

--- CPOutfitters/SocialTypeCell.swift
+++ CPOutfitters/SocialTypeCell.swift
@@ -11,25 +11,34 @@ import Parse
 import ParseUI
 
 class SocialTypeCell: UITableViewCell {
-    @IBOutlet weak var pictureImageView: UIImageView!
     @IBOutlet weak var captionLabel: UILabel!
     @IBOutlet weak var authorLabel: UILabel!
+    
+    @IBOutlet weak var topImageView: PFImageView!
+    @IBOutlet weak var bottomImageView: PFImageView!
+    @IBOutlet weak var footwearImageView: PFImageView!
+    @IBOutlet weak var profileImageView: PFImageView!
     
     var post: PFObject! {
         didSet {
             let author = post["author"] as? PFUser
-            self.authorLabel.text = author?["username"] as? String
+            self.authorLabel.text = author?["fullname"] as? String
             self.captionLabel.text = post["caption"] as? String
             
-            if let imageFile = post["image"] as? PFFile {
-                imageFile.getDataInBackgroundWithBlock { (imageData: NSData?, error: NSError?) in
-                    if error == nil {
-                        if let imageData = imageData {
-                            self.pictureImageView.image = UIImage(data: imageData)
-                        }
-                    }
-                }
-            }
+            profileImageView.file = author?["profilePicture"] as? PFFile
+            
+            //topImageView = post["topImage"] as? PFImageView
+            
+//            topImageView.imageView?.image = 
+//            if let imageFile = post["image"] as? PFFile {
+//                imageFile.getDataInBackgroundWithBlock { (imageData: NSData?, error: NSError?) in
+//                    if error == nil {
+//                        if let imageData = imageData {
+//                            self.pictureImageView.image = UIImage(data: imageData)
+//                        }
+//                    }
+//                }
+//            }
         }
     }
     
